e332624407c3a21ea3b1e99fd035f8f4bdb41c71
Mark Romero <johnncyber@gmail.com>
2010-07-01 05:59:53 +0000

    Changed outcome to combine join table - Updated Schema

--- app/config/db_schema.sql
+++ app/config/db_schema.sql
@@ -15,11 +15,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `matches`
---
-
-DROP TABLE IF EXISTS `matches`;
 CREATE TABLE IF NOT EXISTS `matches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sport_id` int(11) NOT NULL,
@@ -29,12 +24,7 @@ CREATE TABLE IF NOT EXISTS `matches` (
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `sport_id` (`sport_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Matches Table' AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `matches`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Matches Table';
 
 -- --------------------------------------------------------
 
@@ -42,18 +32,14 @@ CREATE TABLE IF NOT EXISTS `matches` (
 -- Table structure for table `outcomes`
 --
 
-DROP TABLE IF EXISTS `outcomes`;
 CREATE TABLE IF NOT EXISTS `outcomes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `people_id` int(11) NOT NULL,
+  `match_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `outcomes`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -61,7 +47,6 @@ CREATE TABLE IF NOT EXISTS `outcomes` (
 -- Table structure for table `people`
 --
 
-DROP TABLE IF EXISTS `people`;
 CREATE TABLE IF NOT EXISTS `people` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -74,33 +59,7 @@ CREATE TABLE IF NOT EXISTS `people` (
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='People Table' AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `people`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `people_matches`
---
-
-DROP TABLE IF EXISTS `people_matches`;
-CREATE TABLE IF NOT EXISTS `people_matches` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `people_id` int(11) NOT NULL,
-  `match_id` int(11) NOT NULL,
-  `outcome_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `people_id` (`people_id`,`match_id`,`outcome_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `people_matches`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='People Table';
 
 -- --------------------------------------------------------
 
@@ -108,7 +67,6 @@ CREATE TABLE IF NOT EXISTS `people_matches` (
 -- Table structure for table `people_sports`
 --
 
-DROP TABLE IF EXISTS `people_sports`;
 CREATE TABLE IF NOT EXISTS `people_sports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `people_id` int(11) NOT NULL,
@@ -117,12 +75,7 @@ CREATE TABLE IF NOT EXISTS `people_sports` (
   PRIMARY KEY (`id`),
   KEY `people_id` (`people_id`),
   KEY `sports_id` (`sports_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='People Spors Relationship Table' AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `people_sports`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='People Spors Relationship Table';
 
 -- --------------------------------------------------------
 
@@ -130,20 +83,12 @@ CREATE TABLE IF NOT EXISTS `people_sports` (
 -- Table structure for table `roles`
 --
 
-DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Roles Table' AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'admin', 'Administrator');
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'user', 'Basic User');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Roles Table';
 
 -- --------------------------------------------------------
 
@@ -151,15 +96,9 @@ INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'user', 'Basic User'
 -- Table structure for table `sports`
 --
 
-DROP TABLE IF EXISTS `sports`;
 CREATE TABLE IF NOT EXISTS `sports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Sports Table' AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `sports`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Sports Table';
